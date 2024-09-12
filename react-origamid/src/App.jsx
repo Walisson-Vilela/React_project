91d68a3ed061ccab4baa55650e45d9d91d40bbd1
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-09-12 17:42:10 -0300

    [FEAT] = Commit changes
    Commit changes

--- react-origamid/src/App.jsx
+++ react-origamid/src/App.jsx
@@ -1,10 +1,92 @@
+import { useState } from "react";
 import "./App.css";
+import Radio from "./assets/Form/Radio";
 
-
+const perguntas = [
+  {
+    pergunta: "Qual método é utilizado para criar componentes?",
+    options: [
+      "React.makeComponent()",
+      "React.createComponent()",
+      "React.createElement()",
+    ],
+    resposta: "React.createElement()",
+    id: "p1",
+  },
+  {
+    pergunta: "Como importamos um componente externo?",
+    options: [
+      'import Component from "./Component"',
+      'require("./Component")',
+      'import "./Component"',
+    ],
+    resposta: 'import Component from "./Component"',
+    id: "p2",
+  },
+  {
+    pergunta: "Qual hook não é nativo?",
+    options: ["useEffect()", "useFetch()", "useCallback()"],
+    resposta: "useFetch()",
+    id: "p3",
+  },
+  {
+    pergunta: "Qual palavra deve ser utilizada para criarmos um hook?",
+    options: ["set", "get", "use"],
+    resposta: "use",
+    id: "p4",
+  },
+];
 function App() {
-  
+  const [respostas, setRespostas] = useState({
+    p1: "",
+    p2: "",
+    p3: "",
+    p4: "",
+  });
+  const [slide, setIslide] = useState(0);
+  const [resultado, setResultado] = useState(null);
+
+  function handleChange({ target }) {
+    setRespostas({ ...respostas, [target.id]: target.value });
+  }
+
+  function resultadoFinal() {
+    console.log("Final");
+    const corretas = perguntas.filter(
+      ({ id, resposta }) => respostas[id] === resposta
+    );
+    setResultado(
+      `Voce acertou ${corretas.length} de ${perguntas.length} perguntas. `
+    );
+    console.log(corretas);
+  }
+
+  function handleClick() {
+    if (slide < perguntas.length - 1) {
+      setIslide(slide + 1);
+    } else {
+      setIslide(slide + 1);
+      resultadoFinal();
+    }
+  }
+
   return (
-   <>Testando...</>
+    <form onSubmit={(event) => event.preventDefault()}>
+      {perguntas.map((pergunta, index) => (
+        <Radio
+          active={slide === index}
+          value={respostas[pergunta.id]}
+          key={pergunta[pergunta.id]}
+          {...pergunta}
+          onchange={handleChange}
+        />
+      ))}
+      {resultado ? (
+        <p>{resultado}</p>
+      ) : (
+        <button onClick={handleClick}>Próximo</button>
+      )}
+    </form>
   );
 }
 
